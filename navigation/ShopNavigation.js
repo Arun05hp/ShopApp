dac5a8882e23dc33ed78009850b1c6db8874488b
Arun Kumar <kumar05.962013@gmail.com>
2020-07-11 17:48:09 +0530

    cart screen

--- navigation/ShopNavigation.js
+++ navigation/ShopNavigation.js
@@ -4,16 +4,24 @@ import { createStackNavigator } from "react-navigation-stack";
 import ProductsOverviewScreen from "../screens/shop/ProductsOverviewScreen";
 import Colors from "../constants/Colors";
 import ProductDetailScreen from "../screens/shop/ProductDetailScreen";
+import CartScreen from "../screens/shop/CartScreen";
 
 const ProductNavigator = createStackNavigator(
   {
     ProductsOverview: ProductsOverviewScreen,
     ProductDetail: ProductDetailScreen,
+    Cart: CartScreen,
   },
   {
     defaultNavigationOptions: {
       headerStyle: {
         backgroundColor: Colors.primary,
+      },
+      headerTitleStyle: {
+        fontFamily: "open-sans-bold",
+      },
+      headerBackTitleStyle: {
+        fontFamily: "open-sans",
       },
       headerTintColor: "white",
     },
---
+++ store/actions/cart.js
@@ -0,0 +1,7 @@
+import Product from "../../models/products";
+
+export const ADD_TO_CART = "ADD_TO_CART";
+
+export const addToCart = (product) => {
+  return { type: ADD_TO_CART, product: product };
+};
