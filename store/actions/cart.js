2d18a233aee79e01717c6e763eac79264730b184
Arun Kumar <kumar05.962013@gmail.com>
2020-07-11 21:39:29 +0530

    remove cart item

--- store/actions/cart.js
+++ store/actions/cart.js
@@ -1,7 +1,10 @@
-import Product from "../../models/products";
-
 export const ADD_TO_CART = "ADD_TO_CART";
+export const REMOVE_FROM_CART = "REMOVE_FROM_CART";
 
 export const addToCart = (product) => {
   return { type: ADD_TO_CART, product: product };
+};
+
+export const removeFromCart = (productId) => {
+  return { type: REMOVE_FROM_CART, pid: productId };
 };
